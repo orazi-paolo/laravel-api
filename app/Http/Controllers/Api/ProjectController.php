a4bea1175166376caf12d4e2189c210c3843ed1b
orazi-paolo <orazi.paolo10@gmail.com>
2024-11-14 15:01:29 +0100

    created Api project controller and implemented method index

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $projects = Project::all();
+        return response()->json([
+            'success' => true,
+            'results' => $projects
+        ]);
+    }
+}
